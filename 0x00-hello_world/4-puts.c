869d26cf314b99a22dcbb9cab4621e8474918c01
ibekennedy <ibeken7878@gmail.com>
2022-09-08 19:33:03 -0700

    my fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+Int main(vold)
+{
+	puts("\"Programming is like building a multingual puzzle");
+	return(0);
+}
+
