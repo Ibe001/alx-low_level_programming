316c39f889185b81838b7138628de22a11e2b47e
ibekennedy <ibeken7878@gmail.com>
2022-09-12 17:31:32 -0700

    second commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the function
+* *main - print the string in the function
+*  *
 *
-* Discription: using the main funtion
-* this program prints "Programming is like building a multilingual puzzle"
-* return: 0
+*   *return: Always 0 (Success)
+*
 */
 int main(void)
 {
-	("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
 
